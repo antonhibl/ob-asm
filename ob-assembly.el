805042fa3fbd7fb02e5695256759412d0502e483
Anton Hibl <antonhibl11@gmail.com>
2023-02-13 21:03:01 -0700

    namespace changes

--- ob-assembly.el
+++ ob-assembly.el
@@ -1,10 +1,10 @@
-;;; ob-arm-asm.el --- Org-babel functions for arm-64 assembly evaluation
+;;; ob-assembly.el --- Org-babel functions for arm-64 assembly evaluation
 
 ;; Copyright (C) 2012 Hibl, Anton
 
 ;; Author: Anton Hibl <antonhibl11@gmail.com>
 ;; Keywords: languages
-;; Homepage: http://github.com/antonhibl/ob-arm-asm
+;; Homepage: http://github.com/antonhibl/ob-assembly
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.1"))
 
@@ -49,54 +49,53 @@
 
 
 ;; optionally define a file extension for this language
-(add-to-list 'org-babel-tangle-lang-exts '("arm-asm" . "arm-asm"))
+(add-to-list 'org-babel-tangle-lang-exts '("assembly" . "assembly"))
 
-(defvar org-babel-default-header-args:arm-asm '())
+(defvar org-babel-default-header-args:assembly '())
 
-(defvar ob-arm-asm-assemble-command "as"
+(defvar ob-assembly-assemble-command "as"
   "The command to use to assemble the arm-64 assembly code.")
 
-(defvar ob-arm-asm-link-command "ld"
+(defvar ob-assembly-link-command "ld"
   "The command to use to link the arm-64 assembly code.")
 
 ;; This macro checks if the given value is a list and returns it as is or
 ;; wrapped in a list if it's not a list.
-(defmacro ob-arm-asm-as-list (val)
+(defmacro ob-assembly-as-list (val)
   "Macro to check if value is in a list, return it in the list if not.
 Argument VAL value to check against list."
   (list 'if (list 'listp val) val (list 'list val)))
 
-(defun org-babel-execute:arm-asm (body params)
+(defun org-babel-execute:assembly (body params)
   "Execute a block of arm-64 assembly code with org-babel.
 This function is
 called by `org-babel-execute-src-block'
 Argument BODY source block body.
-Argument PARAMS parameters for org babel."
+Argument PARAMS source block parameters."
   (message "assembling arm-64 source code block")
-  ;; Assemble the source block using org-babel-temp-file function to create
+  ;; 1. Assemble the source block using org-babel-temp-file function to create
   ;; a temporary file for the assembly code.
   (let ((assembled-file (org-babel-temp-file "arm-asm-")))
     (with-temp-file assembled-file
       (insert body)
-      (shell-command (format "%s -o %s %s" ob-arm-asm-assemble-command
+      (shell-command (format "%s -o %s %s" ob-assembly-assemble-command
                              assembled-file assembled-file)))
-    ;; Link the object file generated from assembling the code with
+    ;; 2. Link the object file generated from assembling the code with
     ;; org-babel-temp-file to create a temporary executable file.
     (let ((executable-file (org-babel-temp-file "arm-asm-")))
-      (shell-command (format "%s -o %s %s" ob-arm-asm-link-command
+      (shell-command (format "%s -o %s %s" ob-assembly-link-command
                              executable-file assembled-file))
-      ;; Run the temporary executable file and return the output as results
+      ;; 3. Run the temporary executable file and return the output as results
       ;; for org-babel.
       (org-babel-eval (concat "./" executable-file) ""))))
 
-
 ;; This function should be used to assign any variables in params in
 ;; the context of the session environment.
-(defun org-babel-prep-session:arm-asm (session params)
+(defun org-babel-prep-session:assembly (session params)
   "This function does nothing as arm-64 assembly is an assembled language.
 Argument SESSION irrelevant for arm-64.
 Argument PARAMS parameters for org-babel."
   (error "Arm-64 assembly is an assembled language -- no support for sessions"))
 
-(provide 'ob-arm-asm)
-;;; ob-arm-asm.el ends here
+(provide 'ob-assembly)
+;;; ob-assembly.el ends here
